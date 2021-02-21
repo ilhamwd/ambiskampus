f79a0201ca73256d77daa2dcdf73d008d95a1ed8
ilhamwd <iilhamwd@gmail.com>
2021-02-21 13:11:45 +0700

    Aku nggawe button wes rampong

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,23 @@ import 'package:ambiskampus_flutter/reusable-components/button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+ void main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.orange, statusBarBrightness: Brightness.dark));
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-        ),
         backgroundColor: Colors.orange,
         body: Center(
           child: DefaultButton(
+            fill: true,
             onTap: () {},
-            value: "Test Button",
+            value: "ASU",
           ),
         ),
       ),
--- lib/reusable-components/button.dart
+++ lib/reusable-components/button.dart
@@ -1,36 +1,65 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
-class DefaultButton extends StatelessWidget {
+class DefaultButton extends StatefulWidget {
   String value;
   Function onTap;
+  bool fill;
 
-  DefaultButton({this.onTap, this.value});
+  DefaultButton({this.onTap, this.value, this.fill: false});
+
+  @override
+  _DefaultButton createState() =>
+      _DefaultButton(onTap: onTap, value: value, fill: fill);
+}
+
+class _DefaultButton extends State {
+  String value;
+  Function onTap;
+  bool _down = false, fill;
+  Duration defaultDuration = Duration(milliseconds: 300);
+
+  _DefaultButton({this.onTap, this.value, this.fill});
+
+  cancel() {
+    this.setState(() {
+      _down = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-        onTap: this.onTap,
-        child: Container(
+    return GestureDetector(
+        onTapDown: (TapDownDetails details) {
+          this.setState(() {
+            _down = true;
+          });
+        },
+        onTapCancel: cancel,
+        onTapUp: (TapUpDetails details) {
+          cancel();
+        },
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 100),
+          width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
             color: Colors.white,
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(.06),
-                offset: Offset(0, 3),
-                blurRadius: 5,
-                spreadRadius: 1,
+                color: Colors.black.withOpacity(.1),
+                offset: !_down ? Offset(3, 3) : Offset(1, 1),
+                blurRadius: !_down ? 8.0 : 1.0,
+                spreadRadius: 0,
               )
             ],
-            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderRadius: BorderRadius.all(Radius.circular(5)),
           ),
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
             child: Text(
-              this.value.toUpperCase(),
+              value.toUpperCase(),
+              textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 16,
+                fontSize: 15,
                 color: Colors.blueGrey.withOpacity(.85),
                 fontWeight: FontWeight.bold,
               ),
